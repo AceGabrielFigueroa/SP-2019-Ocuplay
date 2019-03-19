d064b9960faf2511616765eba9360c001e99ab3e
NinjaCatBob <ace.g.fig@gmail.com>
2019-03-18 23:39:48 -0700

    Revamped GUI & Functions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,39 +18,40 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked()
+// Logic for creating an image
+void MainWindow::on_btn_create_img_clicked()
 {
     // TODO: error check to see if line_box is an actually integer
     //
 
-    int width=ui->image_w->text().toInt();
-    int height=ui->image_h->text().toInt();
+    int width=ui->le_img_wd->text().toInt();
+    int height=ui->le_img_h->text().toInt();
     qDebug() << width;
     qDebug() << height;
 
     CheckerBoard checker1 = CheckerBoard(width, height,
-                                         ui->lr1c1->text().toInt(), ui->lr1c2->text().toInt(), true);
+                                         ui->le_c1->text().toInt(), ui->le_r1->text().toInt(), true);
 
     CheckerBoard checker2 = CheckerBoard(width, height,
-                                         ui->lr2c1->text().toInt(), ui->lr2c2->text().toInt(), true);
+                                         ui->le_c2->text().toInt(), ui->le_r2->text().toInt(), true);
 
     CheckerBoard checker3 = CheckerBoard(width, height,
-                                         ui->lr2c1->text().toInt(), ui->lr2c2->text().toInt(), true);
+                                         ui->le_c3->text().toInt(), ui->le_r3->text().toInt(), true);
 
     CheckerBoard checker4 = CheckerBoard(width, height,
-                                         ui->lr2c1->text().toInt(), ui->lr2c2->text().toInt(), true);
+                                         ui->le_c4->text().toInt(), ui->le_r4->text().toInt(), true);
 
     CheckerBoard checker5 = CheckerBoard(width, height,
-                                         ui->lr2c1->text().toInt(), ui->lr2c2->text().toInt(), true);
+                                         ui->le_c5->text().toInt(), ui->le_r5->text().toInt(), true);
 
     CheckerBoard checker6 = CheckerBoard(width, height,
-                                         ui->lr2c1->text().toInt(), ui->lr2c2->text().toInt(), true);
+                                         ui->le_c6->text().toInt(), ui->le_r6->text().toInt(), true);
 
     CheckerBoard checker7 = CheckerBoard(width, height,
-                                         ui->lr2c1->text().toInt(), ui->lr2c2->text().toInt(), true);
+                                         ui->le_c7->text().toInt(), ui->le_r7->text().toInt(), true);
 
     CheckerBoard checker8 = CheckerBoard(width, height,
-                                         ui->lr2c1->text().toInt(), ui->lr2c2->text().toInt(), true);
+                                         ui->le_c8->text().toInt(), ui->le_r8->text().toInt(), true);
 
     int *result = new int[width * height];
     QString s;
@@ -74,6 +75,15 @@ void MainWindow::on_pushButton_clicked()
     MainWindow::createImage(result, height, width);
 }
 
+// This display an image onto the QLabel
+void MainWindow::displayImage(QImage img) {
+    ui->lbl_display->setPixmap(QPixmap::fromImage(img).scaled(ui->lbl_display->width(),
+                                                              ui->lbl_display->height(),
+                                                              Qt::KeepAspectRatio,
+                                                              Qt::SmoothTransformation));
+}
+
+// This creates an image in memory. Can save if wanted.
 void MainWindow::createImage(int *array, int height, int width) {
 
     std::ifstream _COLOR_TABLE_FILE("C:\\Users\\acegf\\Documents\\QT\\Visual\\resources\\color.txt");
@@ -98,8 +108,44 @@ void MainWindow::createImage(int *array, int height, int width) {
             for (int j = 0; j < width; j++)
                 image.setPixelColor(i, j, _COLOR_TABLE[array[(i * height) + j]]);
 
+    /*
     QString fileName = QFileDialog::getSaveFileName(this, tr("Save Image File"),
                                                         QString(),
                                                         tr("Images (*.bmp)"));
     image.save(fileName);
+    */
+
+    displayImage(image);
+}
+
+// Notifies the user if the input is accepted or not
+void MainWindow::on_le_r1_textChanged(const QString &arg1)
+{   bool* check = new bool(false);
+
+    arg1.toInt(check);
+
+    if(*check) {
+        if(checkNum(ui->le_img_h->text().toInt(),
+                    ui->le_r1->text().toInt())) {
+            ui->le_r1->setStyleSheet("background-color: lightgreen");
+        } else {
+         ui->le_r1->setStyleSheet("background-color: lightred");
+        }
+    } else {
+        ui->le_r1->setStyleSheet("background-color: lightgray");
+    }
+
+    delete check;
+}
+
+// Checks if the num is a factor of it
+// Mainly used to check if the user inputs a width/height
+// is divisible by the col/row
+bool MainWindow::checkNum(int a, int b) {
+    if(a % b) {
+        return false;
+    }
+    else {
+        return true;
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -16,11 +16,15 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_pushButton_clicked();
+    void on_btn_create_img_clicked();
+
+    void on_le_r1_textChanged(const QString &arg1);
 
 private:
     Ui::MainWindow *ui;
+    bool checkNum(int a, int b);
     void createImage(int array[], int height, int width);
+    void displayImage(QImage img);
 };
 
 #endif // MAINWINDOW_H
